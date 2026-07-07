4762d681838138873d30b6c175df6224baca7ecf
Sanjaykumar P <sanj213456@gmail.com>
2026-07-07 13:03:56 +0530

    Update main

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,42 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+
+        localStorage.removeItem("token");
+
+        navigate("/");
+    }
+
+    return (
+
+        <div style={{padding:"30px"}}>
+
+            <h1>Student Management Dashboard</h1>
+
+            <br/>
+
+            <Link to="/add">
+                <button>Add Student</button>
+            </Link>
+
+            <button
+                onClick={logout}
+                style={{marginLeft:"20px"}}
+            >
+                Logout
+            </button>
+
+            <hr/>
+
+            <h2>Students List Coming Next...</h2>
+
+        </div>
+
+    );
 }
 
 export default Dashboard;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,76 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+import "./Login.css";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const res = await API.post("/auth/login", form);
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful");
+
+      navigate("/dashboard");
+    } catch (err) {
+      alert(err.response?.data?.message || "Login Failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Admin Login</h1>
-      <p>Login page coming next...</p>
+    <div className="login-container">
+      <form className="login-card" onSubmit={handleSubmit}>
+
+        <h1>Student Management</h1>
+
+        <h2>Admin Login</h2>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">
+          {loading ? "Logging in..." : "Login"}
+        </button>
+
+      </form>
     </div>
   );
 }
